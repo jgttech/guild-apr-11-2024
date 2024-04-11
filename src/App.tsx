cfbd24fe43d2e3d67483500cc5a53fa15ab2fae8
Jonathon Tech <jonathon.tech@icfnext.com>
2024-04-11 10:03:07 -0500

    WIP

--- src/App.tsx
+++
@@ -1,3 +0,0 @@
-export function App() {
-  return <div className="bg-red-500 pl-[101px]">Hello World</div>;
-}
---
+++ src/routes/index.tsx
@@ -0,0 +1,3 @@
+export const Component: FC = () => {
+  return <div className="bg-red-500">Hello world!</div>;
+};
