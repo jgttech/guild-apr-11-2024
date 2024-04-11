a57e395be8b7e512584aa557e6f8def3902985e8
Jonathon Tech <jonathon.tech@icfnext.com>
2024-04-11 10:08:40 -0500

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import router from "./router";
 
 const root = document.getElementById("root")!;
 
-ReactDOM.createRoot(root).render(<App />);
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
